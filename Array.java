92d226cc960a4563c1d11ee2417affbab0c2f1cd
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-03-14 23:26:10 +0530

    solved

---
+++ Array.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String arg []){
+        int A[]=new int[5];
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<A.length;i++){
+            int x=i+2;
+            System.out.println(x);
+        }
+    }
+}
