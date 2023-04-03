86c6be1392a9bb7c40e4786a343758b63922eab0
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-04-03 16:13:11 +0530

    data hiding

---
+++ Datahidingrectangle.java
@@ -0,0 +1,43 @@
+
+class Rectangle{
+    private int length;
+    private int breadth;
+   
+    public int getlength(){
+        return length;
+    }
+    public int getbreadth(){
+        return breadth;
+    }
+    public void setlength(int l){
+        length=l;
+    }
+    public void setbreadth(int b){
+        length=b;
+    }
+    public int area(){
+        return length*breadth;
+    }
+    public int perimeter(){
+        return 2*(length+breadth);
+    }
+    public boolean isSquare(){
+        if (length==breadth)
+        return true;
+        else return false;
+    }
+
+}
+
+public class Datahidingrectangle {
+    public static void main(String arg[]) {
+        
+    
+    Rectangle r= new Rectangle();
+    r.setlength(58);
+    r.setbreadth(6);
+    System.out.println("area: "+ r.area() );
+    System.out.println("perimeter: "+ r.perimeter() );
+    System.out.println("Square: "+ r.isSquare() );
+    }
+}
