5b12de4f6b8878f0284e3c6211b6f29b0f07f515
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-06-28 22:29:15 +0530

    gui

--- DilogeBox.java
+++ DilogeBox.java
@@ -13,7 +13,25 @@ public class DilogeBox {
         String numberOfYearString= JOptionPane.showInputDialog("Enter number of years");
 
         //convert string into integer
-        
+        int numberOfYears= Integer.parseInt(numberOfYearString);
+
+        // Enter loan amount
+        String loanString=JOptionPane.showInputDialog("enter loan amount");
+        // string into double
+        double loanAmount = Double.parseDouble(loanString);
+
+        // calculate payment
+        double monthlyPayment= loanAmount*monthlyInterestRate/(1-1/Math.pow(1+monthlyInterestRate,
+         numberOfYears*12));
+
+         double totalPayment= monthlyPayment*numberOfYears*12;
+
+         // two digit after the decimal point
+         monthlyPayment=(int)(monthlyPayment*100)/100.0;
+         totalPayment=(int)(totalPayment*100)/100.0;
+         // display result
+         String output= "The monthly payment is " + monthlyPayment +"\n The total Payment is" + totalPayment;
+         JOptionPane.showMessageDialog(null,  output);
 
     }
 }
