225f33afc9c567082f2b906cd76fa5c5ce1a172a
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-07-24 09:07:04 +0530

    the search key

--- TheArray.java
+++ TheArray.java
@@ -4,5 +4,25 @@ public class TheArray {
         arr= new long[100];  // make array
         int nElems= 0; // number of items
         int j; // loop counter
+        long serchkey; // key of items to search
+        //insert 10 items
+        arr[0]=77;
+        arr[1]=99;
+        arr[2]=44;
+        arr[3]=55;
+        arr[4]=22;
+        arr[5]=88;
+        arr[6]=11;
+        arr[7]=00;
+        arr[8]=66;
+        arr[9]=33;
+        nElems=10;  // now 10 elements in array
+
+        // display items
+        for(j=0; j<nElems;j++)
+        System.out.println(arr[j] + " ");
+        System.out.println(" ");
+
+        
     }
 }
