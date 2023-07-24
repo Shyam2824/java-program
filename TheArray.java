fb3fca1c4bcc8a1458c472ae62aea1e755ce9c46
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-07-24 09:01:54 +0530

    to added array

--- TheArray.java
+++ TheArray.java
@@ -1,3 +1,8 @@
 public class TheArray {
-    
+    public static void main(String[] args) {
+        long [] arr;  // reference to array
+        arr= new long[100];  // make array
+        int nElems= 0; // number of items
+        int j; // loop counter
+    }
 }
