c37657b1d060694fadca4273a8cf7ede1d4c385a
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-09 00:27:04 +0530

    if-else condition

--- FindTheNumber.java
+++ FindTheNumber.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class FindTheNumber {
-    
+    public static void main(String[] args) {
+        //System.out.println("Enter the number");
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println("Enter the num1" );
+        int num1 = sc.nextInt();
+        
+        System.out.println("Enter the num1" );
+        int num2 = sc.nextInt();
+        if(num1>num2){
+        System.out.println("greater than num2");
+        }
+        else if(num1==num2)
+        System.out.println("both are equal");
+        else
+        System.out.println("num1 is smaller than num2");
+    }
 }
