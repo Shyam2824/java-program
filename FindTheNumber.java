811607125ca997ee1ef28d444769423146541002
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-09 00:29:58 +0530

    com

--- FindTheNumber.java
+++ FindTheNumber.java
@@ -8,13 +8,15 @@ public class FindTheNumber {
         System.out.println("Enter the num1" );
         int num1 = sc.nextInt();
         
-        System.out.println("Enter the num1" );
+        System.out.println("Enter the num2" );
         int num2 = sc.nextInt();
-        if(num1>num2){
+        System.out.println("Enter the num3" );
+        int num3 = sc.nextInt();
+        if(num1>num2 && num1>num3){
         System.out.println("greater than num2");
         }
-        else if(num1==num2)
-        System.out.println("both are equal");
+        else if(num1==num2 && num2==num3)
+        System.out.println("ALL are equal");
         else
         System.out.println("num1 is smaller than num2");
     }
