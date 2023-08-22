0c155ff7fa611110e191b7914c62c170cb525a29
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-08-22 19:40:24 +0530

    diisible by 9

---
+++ Divisibleby9.java
@@ -0,0 +1,12 @@
+public class Divisibleby9 {
+    public static void main(String[] args) {
+        int i, sum=0;
+        for( i=101;i<200;i++){
+        if(i%9==0){
+            System.out.println(i);
+            //sum+=i;
+        }
+    }
+    }
+    
+}
