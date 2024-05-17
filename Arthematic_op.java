f1508ac445aa1018d70dbe4447239f8b2752be60
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-05-17 23:48:51 +0530

    Arthematic operator

---
+++ Arthematic_op.java
@@ -0,0 +1,21 @@
+public class Arthematic_op {
+    public static void main(String[] args) {
+        int a= 100;
+        int b= 20;
+
+        int add= a+b;
+        System.out.println("Add number : " + add);
+
+        int sub= a-b;
+        System.out.println("Sub number : " + sub);
+
+        int mul= a*b;
+        System.out.println("Mull number : " + mul);
+
+        int div= a+b;
+        System.out.println("Div number : " + div);
+
+        int module =a%b;
+        System.out.println("module number : " + module);
+    }
+}
